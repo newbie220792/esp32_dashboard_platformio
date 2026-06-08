845007b6ce37d97d50536c813089de595ab51db8
Anthony.Doan <anthony.doan@code88.asia>
2026-06-08 11:04:30 +0700

    resolve conflict

--- src/tabs/pi_monitoring_tab/pi_monitoring_tab.h
+++ src/tabs/pi_monitoring_tab/pi_monitoring_tab.h
@@ -15,18 +15,5 @@ public:
     static void ui_update_temp(float value);
 
 private:
-    // static lv_obj_t *cpu_chart;
-    // static lv_obj_t *mem_chart;
-
-    // static lv_chart_series_t *cpu_ser;
-    // static lv_chart_series_t *mem_ser;
-
-    // static lv_obj_t *cpu_label;
-    // static lv_obj_t *mem_label;
-    // static lv_obj_t *temp_label;
-    // static lv_obj_t *temp_bar;
-
     lv_obj_t *create_temp_gauge(lv_obj_t *parent);
-    // bool style_init_done;
-    // static lv_style_t style_indic;
 };
